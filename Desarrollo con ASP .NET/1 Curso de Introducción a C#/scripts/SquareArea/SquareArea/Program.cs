643898b62cf4a709fe839da5d6f06402ae7ce5b8
joelbarrantes <joelbarrantespalacios@gmail.com>
2022-01-16 20:43:04 -0500

    C# Exercise

---
+++ Desarrollo con ASP .NET/1 Curso de Introducción a C#/scripts/SquareArea/SquareArea/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//RECTANGLE CALCULATION
+
+
+//values
+Console.WriteLine("Enter the first and second value");
+float sideA = float.Parse(Console.ReadLine());
+float sideB = float.Parse(Console.ReadLine());
+sideB++; //aumenta el valor en 1
+sideB--; //recude el valor en 1
+
+
+//formula
+float area = sideA * sideB;
+
+Console.WriteLine($"the result is: {area}");
