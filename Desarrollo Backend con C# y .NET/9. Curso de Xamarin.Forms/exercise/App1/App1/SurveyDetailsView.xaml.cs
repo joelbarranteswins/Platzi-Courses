48446ab05aef17526921fbcccec572e0532dfbfb
Joel Barrantes Palacios <58517994+joelbarranteswins@users.noreply.github.com>
2022-07-30 16:12:19 -0500

    Add (Curso Xamarin.Forms): Exercise starting added

---
+++ Desarrollo Backend con C# y .NET/9. Curso de Xamarin.Forms/exercise/App1/App1/SurveyDetailsView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App1.Models;
+using App1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SurveyDetailsView : ContentPage
+    {
+        public SurveyDetailsView()
+        {
+            InitializeComponent();
+            MessagingCenter.Subscribe<SurveyDetailsViewModel, Survey>(this, "SaveSurvey", async (a, s) =>
+            {
+                await Navigation.PopModalAsync();
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<SurveyDetailsViewModel, Survey>(this, "SaveSurvey");
+        }
+    }
+}
---
+++ Desarrollo Backend con C# y .NET/9. Curso de Xamarin.Forms/exercise/App1/App1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class MainPageViewModel : NotificationObject
+    {
+        private ObservableCollection<Survey> surveys;
+        public ObservableCollection<Survey> Surveys
+        {
+            get { return surveys; }
+            set
+            {
+                surveys = value;
+                OnPropertychanged();
+            }
+        }
+
+        public ICommand AddCommand { get; set; }
+
+        public  MainPageViewModel()
+        {
+            Surveys = new ObservableCollection<Survey>();
+            AddCommand = new Command(AddCommandExecute);
+            
+            this.AddCommand = new Command(AddCommandExecute);
+            MessagingCenter.Subscribe<SurveyDetailsViewModel, Survey>(
+                this, "SaveSurvey", (a, s) =>
+                    {
+                        Surveys.Add(s);
+                    });
+        }
+        
+        private void AddCommandExecute()
+        {
+            MessagingCenter.Send(this, "AddSurvey");
+        }
+    
+
+    }
+}
