9d4c4e26d5a8aaf6c4a29be12ed40122999b7a2c
Joel Barrantes Palacios <58517994+joelbarranteswins@users.noreply.github.com>
2023-05-18 02:04:03 -0400

    (Update) folder addes

---
+++ Desarrollo Backend con JavaScript/Curso Básico de JavaScript/scripts/1_variables.js
@@ -0,0 +1,23 @@
+/* Variable es un lugar en memoria
+Es una representación de algún lugar en la memoria para guardar datos
+ */
+
+var nombre = "Oscar";
+console.log(nombre);
+
+// declarar
+var edad; 
+
+//inicializar
+edad = 30;
+
+var elementos = ["Computadora", "Celular"];  
+console.log(elementos);
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+console.log(persona);
+console.log(persona["nombre"]);
