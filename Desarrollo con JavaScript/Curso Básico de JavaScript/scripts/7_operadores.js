bfa10b4db45c7240263415586984db9f2717f7c5
Joel Barrantes Palacios <58517994+joelbarranteswins@users.noreply.github.com>
2023-11-14 18:56:02 -0300

    (Add) Curso Básico de JavaScript moved to aother folder

---
+++ Desarrollo con JavaScript/Curso Básico de JavaScript/scripts/7_operadores.js
@@ -0,0 +1,78 @@
+// operadores aritméticos
+
+// Suma
+console.log(2 + 3) // 5
+// Resta
+console.log(5 - 3) // 2
+// Multiplicación
+console.log(4 * 2) // 8
+// División
+console.log(6 / 2) // 3
+// Módulo (residuo de la división)
+console.log(5 % 2) // 1
+
+// operador de concatenación de strings
+console.log("Hola" + " Platzi") // "Hola Platzi"
+
+
+// operadores de asignación
+var a = 1;
+console.log(a) // 1
+
+var saludo = "Hola Mundo"
+console.log(saludo) // "Hola Mundo"
+
+// operadores de comparación
+// igualdad
+console.log(3 == "3") // true
+console.log(3 == 3) // true
+
+// igualdad estricta (usa el tipo de dato)
+console.log(3 === "3") // false
+console.log(3 === 3) // true
+
+
+// operadores de desigualdad
+console.log(5 != "5") // false
+console.log(5 !== "5") // true
+
+
+// operadores de mayor o menos que
+console.log(5 > 3) // true
+console.log(5 < 3) // false
+console.log(5 >= 6) // false
+console.log(5 <= 6) // true
+
+
+// operadores lógicos
+//operador disyunción lógica
+console.log(true && true) // true
+console.log(true && false) // false
+
+// operador union lógica
+console.log(true || false) // true
+console.log(false || false) // false
+
+// operador de negación lógica
+console.log(!true) // false
+console.log(!false) // true
+
+// operador de incremento y decremento
+var a = 1
+console.log(a++) // 1
+console.log(a) // 2
+
+
+// operador de decremento
+var b = 2   
+console.log(b--) // 2
+console.log(b) // 1
+
+// Operadores de asignación combinada
+var a = 1
+console.log(a += 3) // 4
+console.log(a -= 2) // 2
+
+// operador negación lógico
+var a = 5
+console.log(!(a < 0)) // true
---
+++ Desarrollo con JavaScript/Curso Básico de JavaScript/scripts/8_concional_if.js
@@ -0,0 +1,82 @@
+// Esto nos ayudará para poder hacer decisiones.
+
+var esUsuario = true; 
+
+if(esUsuario) {  // con el if validamos que la condición sea true
+    console.log('Tiene acceso al contenido'); 
+}
+
+// Presentamos el else 
+
+var esUsuario = false; 
+
+if(esUsuario) {
+    console.log('Tiene acceso al contenido'); 
+} else {
+    console.log('Tienes que crear una cuenta para poder acceder al contenido');
+}
+
+// Ejemplo de cómo haríamos una validación
+
+var edad = 18; 
+var accion; 
+
+if(edad >= 18) {
+    accion = 'Puede votar'
+} else { // call back 
+    accion = 'No puede votar'
+}
+
+console.log(accion);
+
+// Ejemplo de cómo utilizaremos el else if
+
+var edad = 18; 
+var accion;  
+
+if(edad === 18) {
+    accion = 'Puede votar, será su 1ra votación'
+} else if(edad > 18) { 
+    accion = 'Puede votar'
+} else { // call back 
+    accion = 'Aun no puede votar'
+}
+
+console.log(accion);
+
+
+
+/* ================================================================ */ 
+
+// Operador ternario 
+
+var numero = 1; 
+var resultado; 
+
+if(numero === 1) {
+    resultado = 'Sí son un 1'; 
+} else {
+    resultado = 'No soy un 1'; 
+}
+
+//condition ? true : false; 
+
+var numero = 1; 
+var resultado = numero === 1 ? 'Sí son un 1' : 'No soy un 1';
+
+console.log(resultado);
+
+
+// ==============================
+
+
+// Ejercicio, reaizar un piedra, papel o tijeras, utilizando condicionales, una funcion que reciba el parametro con el que vamos a jugar, y me regrese si gane o perdí. 
+
+/*  
+
+Variables piedra, papel y tiejeras 
+una funcion que utilice cindicionales para validar si el parametro que le estoy pasando gana o pierde. 
+
+y que la funcion al final me regrese el resultado de si gané, o  perdí. 
+
+*/ 
