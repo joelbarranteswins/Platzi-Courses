53f6982986aea245fbdcb267e0b695e56da4382d
Joel Barrantes Palacios <58517994+joelbarranteswins@users.noreply.github.com>
2024-01-17 20:19:04 -0300

    (Add) Curso de ECMAScript - Historia y Versiones de JavaScript es7 folder added

---
+++ Desarrollo con JavaScript/Curso de ECMAScript - Historia y Versiones de JavaScript/scripts/es7/00-exponential.js
@@ -0,0 +1,2 @@
+const data = 3 ** 4;
+console.log(data);
---
+++ Desarrollo con JavaScript/Curso de ECMAScript - Historia y Versiones de JavaScript/scripts/es7/01-array-includes.js
@@ -0,0 +1,5 @@
+let numbers = [1, 3, 4, 6, 7, 8];
+console.log(numbers.includes(9))
+
+const list = ["oscar", "David", "Ana"]
+console.log(list.includes("Oscar"))
