e7000b0f53ff8fe39b8431656f428bd6a9e1f436
Joel Barrantes Palacios <58517994+joelbarranteswins@users.noreply.github.com>
2024-01-18 13:55:55 -0300

    (Add) Curso de ECMAScript - Historia y Versiones de JavaScript es12 folder added

---
+++ Desarrollo con JavaScript/Curso de ECMAScript - Historia y Versiones de JavaScript/scripts/es12/01-replaceall.js
@@ -0,0 +1,5 @@
+const string = "JavaScript es un maravilloso lenguaje de  programacion";
+
+const replacedString = string.replace("JavaScript", "TypeScript");
+
+console.log(replacedString);
---
+++ Desarrollo con JavaScript/Curso de ECMAScript - Historia y Versiones de JavaScript/scripts/es12/03-private-methods.js
@@ -0,0 +1,27 @@
+// setters getters
+
+class user {
+    //constructor
+    constructor(name, age) {
+        this.name = name;
+        this.age = age
+    }
+    // metodos
+    #speak() {
+        return "Hello";
+    }
+    greeting() {
+        return `${this.speak()} ${this.name}`;
+    }
+
+    get #uAge() {
+        return this.age;
+    }
+    set #uAge(n) {
+        this.age = n;
+    }
+}
+
+const bebeloper1 = new user ("david", 15);
+console.log(bebeloper1.uAge);
+console.log(bebeloper1.uAge = 20);
